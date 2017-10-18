bdbfc69d3495d085ebd39d1bdf269664e455b587
r_lito <rui.mosquito@gmail.com>
2017-10-18 20:59:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artist){
-  delete playlist.artist
+  delete playlist[artistName]
   return playlist
 }
