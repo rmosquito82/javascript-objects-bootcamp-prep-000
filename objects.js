7e0228f39e6585bbeb74b83152b7a218f2750ed2
r_lito <rui.mosquito@gmail.com>
2017-10-18 20:43:45 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = new Object()
+
+function updatePlaylist(playlist, artist, song){
+  playlist.artist = song
+  return playlist
+}
+
+function removePlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
+
+
